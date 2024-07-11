c5a38739b5393ce386178e031006e5efa69a4b42
DESKTOP-VA58A1P\Mateusz <ogonekmateusz69@gmail.com>
2024-07-12 00:34:28 +0200

    add  navigation html code and base styles in navigation.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Navigation from "./components/Navigation";
+import "./assets/navigation.css";
+
 function App() {
   return (
     <Router>
-      <div className="app">{/* navbar */}</div>
+      <div className="app">
+        <Navigation />
+      </div>
       <div className="content">
         <Switch>
           <Route exact path="/">
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeNavbar = () => {
+    setNavbarOpen(false);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-sm  " >
+      <div className="container">
+        {/* <Link to="/" className="navbar-brand">agamua.</Link> */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarsExample04"
+          onClick={toggleNavbar}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarsExample04" className={`collapse navbar-collapse ${navbarOpen ? 'show' : ''}`}>
+          <ul className="navbar-nav ms-auto d-flex">
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={closeNavbar}>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={closeNavbar}>About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={closeNavbar}>Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={closeNavbar}>Works</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact" onClick={closeNavbar}>Contact</Link>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
