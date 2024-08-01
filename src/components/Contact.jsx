373b3fe0d200becd1a55c0158b18b5a6188dd3d4
DESKTOP-VA58A1P\Mateusz <ogonekmateusz69@gmail.com>
2024-08-01 14:07:00 +0200

    add some fixes to css code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,15 @@
+import React from "react";
+
 const Contact = () => {
   return (
-    <>
-      <header className="header d-flex justify-content-center align-items-center mb-5">
-        <h1>Contact Me</h1>
-      </header>
-
+    <div className="contactContent">
       <div className="formBox d-flex container">
         <div className="leftSide">
           <h1>
             I would love to hear from you. Feel free to reach out to me
             directly.
           </h1>
-          <p className="lead">
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
             magni natus corporis doloribus ipsa perspiciatis, vero autem quo
             minus distinctio minima? Modi nam soluta odit corrupti ullam
@@ -22,45 +20,58 @@ const Contact = () => {
           <form>
             <input
               type="text"
-              id="title"
-              name="title"
+              id="firstName"
+              name="firstName"
               required
               className="short"
               placeholder="First Name"
             />
             <input
               type="text"
-              id="title"
-              name="title"
+              id="lastName"
+              name="lastName"
               required
               className="short"
               placeholder="Last Name"
             />
             <input
               type="email"
-              id="title"
-              name="title"
+              id="email"
+              name="email"
               placeholder="Email Address"
               required
             />
             <input
               type="text"
-              id="title"
-              name="title"
+              id="subject"
+              name="subject"
               placeholder="Subject"
               required
             />
-
             <textarea
-              id="body"
-              name="body"
+              id="message"
+              name="message"
               required
               placeholder="Your Message"
             ></textarea>
+            <button type="submit" className="btn">
+              Submit
+            </button>
           </form>
         </div>
       </div>
-    </>
+      {/* <div className="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9944.121073696197!2d-0.20317336773824077!3d51.45760069883039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487605f77875968d%3A0xc2961d78b6fc068a!2sWandsworth%2C%20Londyn%2C%20Wielka%20Brytania!5e0!3m2!1spl!2spl!4v1722504407595!5m2!1spl!2spl"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div> */}
+    </div>
   );
 };
 
